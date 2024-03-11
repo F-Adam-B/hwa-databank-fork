ebce7f422f4cc0da6d44c6ba6597c3b36ba1d32f
f-adam-brad <f.adambrad@gmail.com>
2024-03-11 17:47:43 -0600

    cleanup

--- frontend/hwa-react-app/src/routes/pages/HomePage.tsx
+++ frontend/hwa-react-app/src/routes/pages/HomePage.tsx
@@ -3,18 +3,11 @@ import { GridValidRowModel } from '@mui/x-data-grid';
 
 import { DataGridMUI, MapBox, NavBar, SideBar } from '../../components';
 import { useGetSamplesQuery } from '../../features/samples/samplesApi';
-import {
-  GET_ALL_SAMPLES,
-  GET_ANALYTES,
-} from '../../graphql/queries/sampleQueries';
+import { GET_ALL_SAMPLES } from '../../graphql/queries/sampleQueries';
 import { useQuery } from '@apollo/client';
 const HomePage = () => {
   // const { data, isFetching, isLoading } = useGetSamplesQuery();
   // const { loading, data } = useQuery(GET_ALL_SAMPLES);
-
-  const { loading, data } = useQuery(GET_ANALYTES);
-
-  if (loading) return <div>Loading...</div>;
 
   const columns = [
     { field: 'sampleNumber', headerName: 'Sample Number', width: 150 },
