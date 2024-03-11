dc3c1b41ce7ec9f481e27076112c70c000fcd4ac
f-adam-brad <f.adambrad@gmail.com>
2024-03-11 17:46:13 -0600

    refactor CharForm to be more dynamic

--- frontend/hwa-react-app/src/components/Forms/CharacteristicsForm.tsx
+++ frontend/hwa-react-app/src/components/Forms/CharacteristicsForm.tsx
@@ -1,75 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Controller,
+  useFieldArray,
   useForm,
   SubmitHandler,
   useFormContext,
 } from 'react-hook-form';
-import { Box, Card, MenuItem, Select, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Card,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { ControlledInputField } from '../index';
+import { Analyte, Characteristic } from './SampleForm';
 
-const CharacteristicsForm = () => {
-  const { control } = useFormContext();
+type TCharacteristicsFormProps = {
+  control: any;
+  apiAnalytes: {
+    analyteName: string;
+    characteristics: Characteristic[];
+  }[];
+  register: any;
+  handleCharacteristicStateChange: any;
+};
+
+type Field = {
+  id?: string;
+  analyteName: string;
+  characteristics: Characteristic[];
+  onChange?: any;
+};
+
+const defaultAnalyte: Analyte = {
+  analyteName: '',
+  characteristics: [{ name: '', value: '' }],
+};
+
+const CharacteristicsForm = ({
+  apiAnalytes = [defaultAnalyte],
+  control,
+  handleCharacteristicStateChange,
+  register,
+}: TCharacteristicsFormProps) => {
+  // Function to handle the input change for characteristics
+  const handleCharacteristicChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    analyteName: string,
+    charIndex: number,
+    parentIndex: number
+  ): void => {
+    const newCharValue = e.target.value;
+    handleCharacteristicStateChange((prevState: Analyte[]) => {
+      const updatedAnalytes = [...prevState];
+
+      const updatedCharacteristics = updatedAnalytes[
+        parentIndex
+      ].characteristics.map((characteristic: Characteristic, idx: number) =>
+        idx === charIndex
+          ? { ...characteristic, value: newCharValue }
+          : characteristic
+      );
+
+      updatedAnalytes[parentIndex] = {
+        analyteName,
+        characteristics: updatedCharacteristics,
+      };
+
+      return updatedAnalytes;
+    });
+  };
 
   return (
     <Box>
-      <Typography variant="h5">Characteristics Form</Typography>
       <Card>
-        <Controller
-          name="characteristicGroup"
-          control={control}
-          render={({ field }) => (
-            <ControlledInputField
-              control={control}
-              label="Characteristic Group"
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name="characteristic"
-          control={control}
-          render={({ field }) => (
-            <ControlledInputField
-              control={control}
-              label="Characteristic"
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name="sampleFraction"
-          control={control}
-          render={({ field }) => (
-            <ControlledInputField
-              control={control}
-              label="Sample Fraction"
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name="resultMeasureQualifier"
-          control={control}
-          render={({ field }) => (
-            <ControlledInputField
-              control={control}
-              label="Characteristics Measured"
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name="result"
-          control={control}
-          render={({ field }) => (
-            <ControlledInputField
-              control={control}
-              label="Activity Media"
-              {...field}
-            />
-          )}
-        />
+        {apiAnalytes.map((analyte: Field, parentIndex: number) => (
+          <div key={analyte.id}>
+            <Typography variant="h5">{analyte.analyteName}</Typography>
+            <Box
+              component="div"
+              sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+              }}
+            >
+              {analyte.characteristics.map(
+                (characteristic: Characteristic, charIndex) => {
+                  return (
+                    <TextField
+                      key={`char-${charIndex}`}
+                      type="text"
+                      value={characteristic.value}
+                      onChange={(e) =>
+                        handleCharacteristicChange(
+                          e,
+                          analyte.analyteName,
+                          charIndex,
+                          parentIndex
+                        )
+                      }
+                      placeholder={characteristic.name}
+                      label={characteristic.name}
+                    />
+                  );
+                }
+              )}
+            </Box>
+          </div>
+        ))}
       </Card>
     </Box>
   );
