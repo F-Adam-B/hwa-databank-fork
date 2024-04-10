95da456df59e8c140c8f49795fccc3b40b28d47c
f-adam-brad <f.adambrad@gmail.com>
2024-04-10 16:03:56 -0600

    add mutation to delete newsfeed

--- backend/graphql/types.js
+++ backend/graphql/types.js
@@ -32,6 +32,7 @@ const typeDefs = gql`
     addSampleMutation(sampleFormValues: SampleFormValuesInputType): Sample
     addUserMutation(userFormValues: UserFormValuesInputType): UserType
     addNewsFeedMutation(newsFeedValues: NewsFeedValuesInputType): NewsFeed
+    deleteNewsFeed(id: ID!): NewsFeed
   }
 
   type AnalyteType {
