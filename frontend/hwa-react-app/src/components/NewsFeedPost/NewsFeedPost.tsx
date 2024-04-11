1ad860649053f126c1631d844e19fa79301de5a3
f-adam-brad <f.adambrad@gmail.com>
2024-04-11 16:08:18 -0600

    add delete news feed mutation only render alerts to admins

--- frontend/hwa-react-app/src/components/NewsFeedPost/NewsFeedPost.tsx
+++ frontend/hwa-react-app/src/components/NewsFeedPost/NewsFeedPost.tsx
@@ -1,10 +1,18 @@
 import { useContext } from 'react';
-import { Card, CardContent, Container, Grid, Typography } from '@mui/material';
+import {
+  Alert,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material';
 import { NewsFeedCard, NewsFeedForm } from '../index';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 
 import { UsersContext } from '../../Providers/UsersContext';
 import { NEWS_FEED_QUERY } from '../../graphql/queries/newFeedQueries';
+import { DELETE_NEWS_FEED_POST } from '../../graphql/mutations/newsFeedMutations';
 import { NewsFeedProps } from '../../graphql/types';
 
 const EmptyNewsFeedDisplay = () => {
@@ -25,15 +33,41 @@ const NewsFeed = () => {
   const listOfUsers = useContext(UsersContext);
 
   const { data, loading } = useQuery(NEWS_FEED_QUERY);
+  const [
+    deleteNewsFeedPost,
+    { data: deleteNewsFeedMutationData, error: deleteNewsFeedMutationError },
+  ] = useMutation(DELETE_NEWS_FEED_POST);
 
-  const handleDeleteNewsFeedPost = (id: string) => {};
+  const handleDeleteNewsFeedPost = async (id: string) => {
+    try {
+      deleteNewsFeedPost({
+        variables: {
+          id,
+        },
+        refetchQueries: [NEWS_FEED_QUERY],
+      });
+    } catch (error) {
+      console.error(`Error deleting news feed post with ID ${id}: error`);
+    }
+  };
 
   if (loading) return <>Loading...</>;
-  if (!data?.newsFeedPosts.length) return <EmptyNewsFeedDisplay />;
   return (
     <>
       <NewsFeedForm />
-      <Grid justifyContent="center" container spacing={1}>
+      {!data?.newsFeedPosts.length && <EmptyNewsFeedDisplay />}
+      {!!deleteNewsFeedMutationData && (
+        <Alert severity="success">News Feed Post successfully deleted</Alert>
+      )}
+      {!!deleteNewsFeedMutationError && (
+        <Alert severity="error">Error deleting news feed post</Alert>
+      )}
+      <Grid
+        justifyContent="center"
+        container
+        spacing={1}
+        sx={{ marginBottom: '2em' }}
+      >
         {data.newsFeedPosts.map(({ authorId, ...rest }: NewsFeedProps) => {
           // TODO: Will need to get actual name from Auth0;
           const authorName = listOfUsers.find(
