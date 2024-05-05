fa0a8443a7be32bf1f6ba8005135467790d16410
f-adam-brad <f.adambrad@gmail.com>
2024-05-05 16:26:49 -0600

    remove unused declarations

--- frontend/hwa-react-app/src/routes/pages/FormPage.tsx
+++ frontend/hwa-react-app/src/routes/pages/FormPage.tsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import {
-  Controller,
-  useForm,
-  SubmitHandler,
-  FormProvider,
-} from 'react-hook-form';
-import { Box, Card, Input, MenuItem, Select, Typography } from '@mui/material';
-import {
-  InformationForm,
-  LocationForm,
-  ResultsForm,
-  SampleForm,
-} from '../../components';
+import { Box } from '@mui/material';
+import { SampleForm } from '../../components';
 
 const FormPage = () => {
   return (
