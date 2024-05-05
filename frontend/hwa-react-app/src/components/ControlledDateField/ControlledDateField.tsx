2a45acfccc009bc9aa15d95ab7cc126170e7b0cf
f-adam-brad <f.adambrad@gmail.com>
2024-05-05 12:28:24 -0600

    remove provider

--- frontend/hwa-react-app/src/components/ControlledDateField/ControlledDateField.tsx
+++ frontend/hwa-react-app/src/components/ControlledDateField/ControlledDateField.tsx
@@ -1,60 +1,39 @@
 import React from 'react';
-import {
-  Path,
-  Control,
-  Controller,
-  useForm,
-  SubmitHandler,
-  useFormContext,
-  FieldValues,
-  RegisterOptions,
-} from 'react-hook-form';
+import { Control, Controller } from 'react-hook-form';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import TextField from '@mui/material/TextField';
-import { render } from '@testing-library/react';
 import { FormHelperText } from '@mui/material';
-
-interface ControlledDateFieldProps {
-  control: Control<any>;
-  label?: string;
-  name: string;
-  value?: Date | null;
-  onChange?: any;
-  required?: boolean;
-}
 
 const ControlledDateField = ({
   control,
   label,
   name,
-  onChange,
   required,
-}: ControlledDateFieldProps) => {
+}: {
+  control: Control<any>;
+  label: string;
+  name: string;
+  required?: boolean;
+}) => {
   return (
     <Controller
       control={control}
       name={name}
       rules={{ required: required ? 'This field is required' : false }}
-      render={({ field, fieldState }) => (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+      render={({ field: { onChange, ...field }, fieldState: { error } }) => (
+        <>
           <DatePicker
             {...field}
             label={label}
             onChange={(newValue) => {
-              field.onChange(newValue);
+              onChange(newValue);
             }}
             // error={Boolean(fieldState.error)} // This will indicate an error visually on the UI
             // helperText={fieldState.error ? fieldState.error.message : null} // Display the error mess
           />
 
-          {fieldState.error && (
-            <FormHelperText>{fieldState.error.message}</FormHelperText>
-          )}
-        </LocalizationProvider>
+          {error && <FormHelperText>{error.message}</FormHelperText>}
+        </>
       )}
     />
   );
