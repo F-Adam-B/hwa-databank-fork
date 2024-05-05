02139c9e29743d57771812c40141ea961c884d3b
f-adam-brad <f.adambrad@gmail.com>
2024-05-05 16:52:08 -0600

    cleanup

--- frontend/hwa-react-app/src/components/SideBar/SideBar.tsx
+++ frontend/hwa-react-app/src/components/SideBar/SideBar.tsx
@@ -1,12 +1,9 @@
-import React, { useState, KeyboardEvent, MouseEvent, Fragment } from 'react';
+import { useState, KeyboardEvent, MouseEvent, Fragment } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 
 type Anchor = 'top' | 'left' | 'bottom' | 'right';
